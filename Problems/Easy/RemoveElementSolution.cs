7e3a76b3d3b096eacbe95e46be3fc072345b3172
bo.furlan@gmail.com <bo.furlan@gmail.com>
2025-01-13 18:34:08 -0300

    Solved RemoveElement

---
+++ Problems/Easy/RemoveElementSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Problems.Easy;
+
+public static class RemoveElementSolution
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+                k++;
+            else
+                nums[i - k] = nums[i];
+        }
+        return nums.Length - k;
+    }
+
+    public static void Solve()
+    {
+        var input = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        var output = RemoveElement(input, 2);
+    }
+}
