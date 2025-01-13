60766d8817ecd89f8e0657847dd718fa94597f8c
bo.furlan@gmail.com <bo.furlan@gmail.com>
2025-01-13 19:03:08 -0300

    Solved LengthOfLastWord

---
+++ Problems/Easy/LengthOfLastWordSolution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeProblems.Problems.Easy;
+
+public static class LengthOfLastWordSolution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        int len = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+                len++;
+            else if (s[i] == ' ' && len > 0)
+                break;
+        }
+        return len;
+    }
+}
