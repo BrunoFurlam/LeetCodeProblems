6dcab22fc42f007091b61ea5245b511e3b6bc95a
bo.furlan@gmail.com <bo.furlan@gmail.com>
2025-01-13 18:57:45 -0300

    Solved IndexOfFirstOccurence

---
+++ Problems/Easy/IndexOfFirstOcurrenceSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeProblems.Problems.Easy;
+
+public static class IndexOfFirstOcurrenceSolution
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            for (int j = 0; j < needle.Length; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                    break;
+                else if (j == needle.Length - 1)
+                    return i;
+            }
+        }
+
+        return -1;
+    }
+}
