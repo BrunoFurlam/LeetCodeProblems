7dd3dccf7879b89c9a8b9478d2fc24aed0e0d81e
bo.furlan@gmail.com <bo.furlan@gmail.com>
2025-01-31 11:46:02 -0300

    Solved SameTree

---
+++ Problems/Easy/SameTreeSolution.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeProblems.Problems.Easy;
+
+public class SameTreeSolution
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p == null || q == null)
+            return p == q;
+
+        return p.val == q.val
+            && IsSameTree(p.left, q.left)
+            && IsSameTree(p.right, q.right);
+    }
+}
